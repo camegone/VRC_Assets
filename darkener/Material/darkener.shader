10785908102704d559f52cd5199071a6a3b86e77
camegone <121595305+camegone@users.noreply.github.com>
2023-03-27 09:05:46 +0900

    Add files via upload

---
+++ darkener/Material/darkener.shader
@@ -0,0 +1,54 @@
+﻿Shader "camegone/darkener"
+{
+    Properties
+    {
+        [MainColor] _UdonDarkenerColor("Overlay Color", Color) = (1.0, 1.0, 1.0, 1.0)
+    }
+        SubShader
+    {
+        Tags {"Queue" = "Transparent+10000"}
+        BlendOp Add
+        Blend Zero SrcColor
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 _UdonDarkenerColor;
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                return _UdonDarkenerColor;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ darkener/Udon/Darkener.cs
@@ -0,0 +1,65 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+using UnityEngine.UI;
+
+namespace camegone.Darkener
+{
+    public class Darkener : UdonSharpBehaviour
+    {
+        public float _brightness = 1.0f;
+        public Color _tint = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        public MeshRenderer _darkener;
+        public Slider _bright;
+        public Slider _red;
+        public Slider _green;
+        public Slider _blue;
+
+        void Start()
+        {
+            UpdateColor();
+        }
+
+        public void UpdateColor() {
+            _darkener.material.SetColor("_UdonDarkenerColor", _tint * VecBright(_brightness));
+        }
+
+        Color VecBright(float b)
+        {
+            return new Color(b, b, b, 1);
+        }
+
+        float GetSliderVal(Slider slider) 
+        {
+            return slider.value;
+        }
+
+        public void OnBrightnessChanged()
+        {
+            _brightness = GetSliderVal(_bright);
+            UpdateColor();
+        }
+
+        public void OnRedChanged()
+        {
+            _tint.r = GetSliderVal(_red);
+            UpdateColor();
+        }
+
+        public void OnGreenChanged()
+        {
+            _tint.g = GetSliderVal(_green);
+            UpdateColor();
+        }
+
+        public void OnBlueChanged()
+        {
+            _tint.b = GetSliderVal(_blue);
+            UpdateColor();
+        }
+
+    }
+}
