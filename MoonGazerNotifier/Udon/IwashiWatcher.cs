bca3c8d60110ae21c7a06f15ada4578cc5eda28e
camegone <121595305+camegone@users.noreply.github.com>
2023-03-29 17:41:51 +0900

    Add files via upload

---
+++ MoonGazerNotifier/Udon/IwashiWatcher.cs
@@ -0,0 +1,77 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+using HoshinoLabs.IwaSync3.Udon;
+
+namespace camegone.MoonGazerNotifier
+{
+    public class IwashiWatcher : VideoControllerEventListener
+    {
+        [SerializeField] Sender _sender;
+        [SerializeField] VideoCore _videocore;
+        [SerializeField] float _interval = 1.0f;
+
+        private float _timerCount = 0.0f;
+        private bool _isUrlChanged = false;
+        private string _messageOld = "";
+
+        const string _title = "Iwasync3VideoPlayer";
+
+        void Start() 
+        {
+            if (_sender == null)
+                _sender = this.GetComponent<Sender>();
+            if (_videocore == null)
+                _videocore = GameObject.Find("Udon (VideoCore)").GetComponent<VideoCore>();
+
+            _videocore.AddListener(this);
+        }
+
+        public override void OnChangeURL()
+        {
+            _isUrlChanged = true;
+        }
+
+        private void Update()
+        {
+            if (_isUrlChanged && _videocore.isPlaying)
+            {
+                if (_interval <= _timerCount)
+                {
+                    _timerCount = 0.0f;
+                    if (!_messageOld.Equals(_videocore.Message))
+                    {
+                        OnChangeVideo();
+                    }
+                }
+                else
+                {
+                    _timerCount += Time.deltaTime;
+                }
+            }
+        }
+
+        private void OnChangeVideo()
+        {
+            _sender.SendNotification("Now Playing", FormattedMessage());
+            _messageOld = _videocore.Message;
+            _isUrlChanged = false;
+            _timerCount = 0.0f;
+        }
+
+        private string FormattedMessage()
+        {
+            if (_videocore.Message == "")
+                return _videocore.url.ToString();
+            else
+            {
+                return _videocore.Message;
+            }
+        }
+
+    }
+}
---
+++ MoonGazerNotifier/Udon/Sender.cs
@@ -0,0 +1,87 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace camegone.MoonGazerNotifier
+{
+    public class Sender : UdonSharpBehaviour
+    {
+        [SerializeField] private LWNController _LWN;
+        [SerializeField] private bool _muteNotification = true;
+        [SerializeField] private AudioSource _sourceToMute;
+        
+
+        private readonly float _waitTime = 2.0f;
+        private float _timerCount = 0.0f;
+        private bool _isMuted = false;
+
+        void Start() 
+        {
+            if (_LWN == null)
+                _LWN = GameObject.Find("LunarWorldNotification").GetComponent<LWNController>();
+            if (_sourceToMute == null && _muteNotification)
+                _sourceToMute = GameObject.Find("LunarWorldNotification").GetComponent<AudioSource>();
+        }
+
+        void Update() 
+        {
+            if (_isMuted)
+            {
+                if (_timerCount >= _waitTime)
+                {
+                    _timerCount = 0.0f;
+                    _isMuted = false;
+                    _sourceToMute.enabled = true;
+                }
+                else
+                {
+                    _timerCount += Time.deltaTime;
+                }
+            }
+
+        }
+        
+
+        public void SendNotification(string title, string message)
+        {
+            if (_muteNotification)
+            {
+                _sourceToMute.enabled = false;
+                _isMuted = true;
+            }
+            _LWN.PlayCustomNotification(title, ExcludeTags(message));
+        }
+
+        private readonly string[] simpleTags = { "<b>", "<i>", "</b>", "</i>", "</size>", "</color>", "</material>" };
+        private readonly string[] complexTags = { "<size=", "<color=", "<material=" , "<quad " };
+
+        private string ExcludeTags(string tex) // remove rich text tags
+        {
+            foreach (string tag in simpleTags) // remove simple tags
+            {
+                tex = tex.Replace(tag, "");
+            }
+
+            foreach (string tag in complexTags)
+            {
+                int head = 0;
+                for (int s = tex.IndexOf(tag, head); s >= 0; s = tex.IndexOf(tag, head))
+                {
+                    head = s + 1;
+                    int e = tex.IndexOf(">", head);
+                    if (e < 0)
+                        continue;
+                    else
+                    {
+                        tex = tex.Remove(s, e - s + 1);
+                        head = 0;
+                    }
+                }
+            }
+
+            return tex;
+        }
+    }
+}
